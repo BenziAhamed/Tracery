334ad00db5a51bf4000edc4a758e8343ee251cf9
Benzi Ahamed <to.benzi@gmail.com>
2017-03-14 13:31:55 +0530

    updated travis to only build macOS target with tests

--- CommonTesting/Conditionals.swift
+++ CommonTesting/Conditionals.swift
@@ -13,19 +13,19 @@ class Conditionals: XCTestCase {
     
     func testIfBlock() {
         
-//        let line = "[if #1#==#1# then [tag:you] else [tag:me]]#tag#"
-//        print(Lexer.tokens(input: line))
-        
-        Tracery.logLevel = .verbose
-        let t = Tracery.hierarchical {[
-            "number" : [-1,0,1,2,3,4,5,6,7,8,9],
-            ]}
-        
-        t.setCandidateSelector(rule: "number", selector: SequentialSelector())
-        
-        Tracery.maxStackDepth = 30
-        print(t.expand("[while #number#!=9 do #number#,]"))
-        
+////        let line = "[if #1#==#1# then [tag:you] else [tag:me]]#tag#"
+////        print(Lexer.tokens(input: line))
+//        
+//        Tracery.logLevel = .verbose
+//        let t = Tracery.hierarchical {[
+//            "number" : [-1,0,1,2,3,4,5,6,7,8,9],
+//            ]}
+//        
+//        t.setCandidateSelector(rule: "number", selector: SequentialSelector())
+//        
+//        Tracery.maxStackDepth = 30
+//        print(t.expand("[while #number#!=9 do #number#,]"))
+//        
     }
     
 }
